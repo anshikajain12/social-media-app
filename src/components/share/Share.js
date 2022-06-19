6f8c4534c6fc6c904cd4c55a1aea9a6a3785959e
anshika jain <nisthajain2412@gmail.com>
2022-06-19 12:48:03 +0530

    updated

--- src/components/share/Share.js
+++ src/components/share/Share.js
@@ -1,22 +1,35 @@
 import React from 'react'
 import './share.css';
-import { PermMedia} from "@mui/icons-material";
+import { PermMedia,Label,Room ,EmojiEmotions} from "@mui/icons-material";
 
 function Share() {
   return (
     <div className="share">
         <div className="sharewrapper"></div>
         <div className="sharetop">
-            {/* <img src="/Assests/person/1.jpg" alt="" srcset="" className='shareprofileimage'/> */}
+            <img src="/Assests/person/1.jpg" alt="" srcset="" className='shareprofileimage'/>
             <input placeholder="What's in your mind safak?" className="shareinput" />
         </div>
         <hr className="sharehr" />
         <div className="sharebottom">
             <div className="shareoptions">
-                <PermMedia className="shareicon"/>
+                <PermMedia htmlColor="tomato" className="shareicon"/>
                 <span className="shareoptiontext">Photo or Video</span>
             </div>
+            <div className="shareoptions">
+                <Label  htmlColor="blue" className="shareicon"/>
+                <span className="shareoptiontext">Tag</span>
+            </div>
+            <div className="shareoptions">
+                <Room  htmlColor="green" className="shareicon"/>
+                <span className="shareoptiontext">Locations</span>
+            </div>
+            <div className="shareoptions">
+                <EmojiEmotions  htmlColor="goldenrod" className="shareicon"/>
+                <span className="shareoptiontext">Feelings</span>
+            </div>
         </div>
+        <button className="sharebuttons">Share</button>
     </div>
   )
 }
